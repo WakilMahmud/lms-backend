989d84d3dbf98d0ed3e7c67c39abafc9fe7cce15
Wakil Mahmud <wakilmahmud30@gmail.com>
2025-08-11 00:40:43 +0600

    added  mongodb database using mongoose

--- src/core/app.js
+++ src/core/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('LMS Backend is running.');
+    res.send('LMS Backend Service is running.');
 });
 
 export default app;
--- src/core/server.js
+++ src/core/server.js
@@ -1,7 +1,34 @@
+import mongoose from 'mongoose';
+import config from '../config/index.js';
 import app from './app.js';
 
-const port = 3000;
+let server;
 
-app.listen(port, () => {
-    console.log(`LMS Backend app is running on port ${port}`);
+async function main() {
+    try {
+        await mongoose.connect(config.DATABASE_URL);
+
+        server = app.listen(config.PORT, () => {
+            console.log(`LMS Backend App is running on port ${config.PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+main();
+
+process.on('unhandledRejection', (err) => {
+    console.log(`😈 unahandledRejection is detected , shutting down ...`, err);
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+    }
+    process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+    console.log(`😈 uncaughtException is detected , shutting down ...`);
+    process.exit(1);
 });
